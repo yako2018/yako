dee5d6337d4ca9a5d74a2466f3bf8b3115e062d7
yy <yy>
2017-10-27 15:58:55 +0800

    ex pic

--- test/FoundCity/FoundCity/Controllers/PetController.cs
+++ test/FoundCity/FoundCity/Controllers/PetController.cs
@@ -22,16 +22,16 @@ namespace FoundCity.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult YCreateFindPet(FormCollection frm) {
-            HttpPostedFileBase file1 = Request.Files["PetPhoto"];
+        //[HttpPost]
+        //public ActionResult YCreateFindPet(FormCollection frm) {
+        //    HttpPostedFileBase file1 = Request.Files["PetPhoto"];
 
-            string UploadedPathFileName = Server.MapPath("~/images") + "\\" + file1.FileName;
-            file1.SaveAs(UploadedPathFileName);
-            //Response.Cookies["uploadImg"].Value = file1.FileName;
-            //TempData["PetPhoto"] = file1.FileName;
-            return View();
-            //return RedirectToAction("YCreateFindPet", "Home");
-        }
+        //    string UploadedPathFileName = Server.MapPath("~/images") + "\\" + file1.FileName;
+        //    file1.SaveAs(UploadedPathFileName);
+        //    //Response.Cookies["uploadImg"].Value = file1.FileName;
+        //    //TempData["PetPhoto"] = file1.FileName;
+        //    return View();
+        //    //return RedirectToAction("YCreateFindPet", "Home");
+        //}
     }
 }
