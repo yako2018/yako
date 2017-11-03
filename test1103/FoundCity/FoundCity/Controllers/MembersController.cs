15171b598b665f77e904c81ca0597d0deadfdb87
yy <yy>
2017-11-03 19:00:26 +0800

    y

---
+++ test1103/FoundCity/FoundCity/Controllers/MembersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoundCity.Models;
+namespace FoundCity.Controllers
+{
+    public class MembersController : ApiController
+    {
+        CMemberService memberService = new CMemberService();
+
+        public bool GetMemberId(string MemberId) {
+            /*比對目前會員登入Id是否與View裡的會員Id相同,false = 被竄改*/
+            bool result = memberService.GetHashMemberId(User.Identity.Name).Equals(memberService.GetHashMemberId(MemberId));
+            return result;
+        }
+    }
+}
