413b56c48ce4ead68a2bf3929589137d687da27a
yy <yy>
2017-11-06 09:24:21 +0800

    11

---
+++ test1105/FoundCity/FoundCity/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using FoundCity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoundCity.Controllers {
+    public class HomeController : Controller {
+
+        HomeModel homeModel = new HomeModel();
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Data() {
+            var data = homeModel.Data();
+            return Json(data,JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ test1105/FoundCity/FoundCity/Controllers/MembersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoundCity.Models;
+namespace FoundCity.Controllers
+{
+    public class MembersController : ApiController
+    {
+        CMemberService memberService = new CMemberService();
+        // POST: api/Members/1 simon 11/03 13:22 尚未使用
+        public IHttpActionResult GetMember(string MemberId) {
+            /*比對目前會員登入Id是否與View裡的會員Id相同,false = 被竄改*/
+            bool result = memberService.GetHashMemberId(memberService.GetMemberId(User.Identity.Name)).Equals(memberService.GetHashMemberId(MemberId));
+            var IsResult = Convert.ToString(result);
+            return Ok(IsResult);
+        }
+    }
+}
