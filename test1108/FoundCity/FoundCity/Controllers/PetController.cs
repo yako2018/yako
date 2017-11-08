ddf33cb3d19b6f0e50feecca13b323e18a9f86dd
yy <yy>
2017-11-08 11:50:57 +0800

    y

--- test1108/FoundCity/FoundCity/Controllers/PetController.cs
+++ test1108/FoundCity/FoundCity/Controllers/PetController.cs
@@ -5,11 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using FoundCity.Models;
 using System.Web.Security;
+using FoundCity.ViewModels;
+
 namespace FoundCity.Controllers
 {
     public class PetController : Controller
     {
         CMemberService memberService = new CMemberService();
+        private NewPetEntities db = new NewPetEntities();
         // GET: Pet
         public ActionResult Index()
         {
@@ -22,6 +25,11 @@ namespace FoundCity.Controllers
         }
         [Authorize]
         public ActionResult MemberFindPet() {
+            //為對照isSameData而建
+            if (ModelState.IsValid) {
+                /*取得會員Id*/
+                ViewData["MemberId"] = memberService.GetMemberId(User.Identity.Name);
+            }
             return View();
         }
         [Authorize]
@@ -55,6 +63,37 @@ namespace FoundCity.Controllers
         [HttpPost]
         public ActionResult getMemberData(int MemberId) {
             return Json(memberService.GetBasicMemberData(MemberId));
+        }
+
+        //補
+        [HttpPost]
+        public ActionResult isSameData(int MemberId) {
+            var query = from o in db.FindPets
+                        join t in db.FindMoms on o.HairColor equals t.HairColor
+                        where o.CreatorId == MemberId 
+                        && o.HairColor == t.HairColor
+                        && o.Species == t.Species
+                        && o.Size == t.Size
+                        &&o.LostPlace1 == t.FindPlace1
+                        && o.LostPlace2 == t.FindPlace2
+                        //&& o.LostPlace3 == t.FindPlace3
+                        //&& o.LostDate == t.FindDate
+                        select new PetViewModel {
+                            pid = o.PetId,
+                            pphoto = o.PetPhoto,
+                            mid = t.PetId,
+                            mphoto = t.PetPhoto,
+                            mconName = t.ContactMan,
+                            mconSex = t.ContactSex,
+                            mconTel = t.ContactTel
+                        };
+            //return Content("ok");
+            if (query.Count() > 0) {
+                return Json(query.ToList());
+            } else {
+                return Json("尚未有資料");
+            }
+            
         }
     }
 }
