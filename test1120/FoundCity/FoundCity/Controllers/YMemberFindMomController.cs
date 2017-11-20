4d34228e1811291264824cc4247efda0d438f52e
yako2018 <koyaya8@gmail.com>
2017-11-20 17:41:44 +0800

    1

---
+++ test1120/FoundCity/FoundCity/Controllers/YMemberFindMomController.cs
@@ -0,0 +1,50 @@
+﻿using FoundCity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FoundCity.Controllers
+{
+    public class YMemberFindMomController : ApiController
+    {
+        private NewPetEntities db = new NewPetEntities();
+        CMemberService memberService = new CMemberService();
+
+        public List<FindMom> GetYMemberFindPet() {
+            //CookieHeaderValue cookie = Request.Headers.GetCookies("uid").FirstOrDefault();
+
+            //一個月前的時間
+            //string dueDate = DateTime.Now.AddMonths(-1).ToString("yyyy-MM-dd");
+
+            //CreatorId
+            int uid = Convert.ToInt32(memberService.GetMemberId(User.Identity.Name));
+            var query = from o in db.FindMoms
+                        where o.CreatorId == uid && (o.State == 1)
+                        //先拿掉作對照(顯示一個月內資料)
+                        //&& (string.Compare(dueDate, o.CreateDate) < 0)
+                        orderby o.PetId descending
+                        select o;
+
+            var viewData = query.ToList();
+            return viewData;
+
+
+
+            //CookieHeaderValue cookie = Request.Headers.GetCookies("uid").FirstOrDefault();
+
+            ////CreatorId
+            //int uid = Convert.ToInt32(cookie["uid"].Value);
+            //var query = from o in db.FindMoms
+            //            where o.CreatorId == uid
+            //            orderby o.PetId descending
+            //            select o;
+
+            //var viewData = query.ToList();
+            //return viewData;
+
+        }
+    }
+}
---
+++ test1120/FoundCity/FoundCity/ViewModels/PetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoundCity.ViewModels {
+    public class PetViewModel {
+        public int pid { get; set; }
+        public string pphoto { get; set; }
+        //public Nullable<int> intData { get; set; }
+        public int mid { get; set; }
+        public string mphoto { get; set; }
+        public string mconName { get; set; }
+        public string mconSex { get; set; }
+        public string mconTel { get; set; }
+    }
+}
